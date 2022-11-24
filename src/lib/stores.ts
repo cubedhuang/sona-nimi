a41cbd86c776e321e3a5b28c5afd58bb9571a1ae
cubedhuang <99danielh@gmail.com>
2022-11-23 23:39:30 -0500

    take user dark pref by default unless overridden

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,13 +1,14 @@
 import { browser } from '$app/environment';
 import { writable } from 'svelte/store';
 
-export const darkMode = writable(
-	browser ? localStorage.getItem('darkMode') === 'true' : false
-);
+export const darkMode = writable(true);
 
 if (browser) {
+	const stored = localStorage.getItem('darkMode');
+	if (stored) darkMode.set(stored === 'true');
+
 	darkMode.subscribe(value => {
-		localStorage.setItem('darkMode', `${value}`);
+		localStorage.setItem('darkMode', value.toString());
 
 		document.documentElement.classList.toggle('dark', value);
 	});
