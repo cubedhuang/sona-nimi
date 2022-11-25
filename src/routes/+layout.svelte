70623de00d3ecb698e08f721f2df7d4ca577ffd8
cubedhuang <99danielh@gmail.com>
2022-11-25 01:01:34 -0500

    prefetch links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,6 +17,9 @@
 	<meta property="og:url" content="https://nimi.dan.onl/" />
 </svelte:head>
 
-<div class="px-8 py-16 lg:px-16 max-w-screen-2xl m-auto">
+<div
+	class="px-8 py-16 lg:px-16 max-w-screen-2xl m-auto"
+	data-sveltekit-prefetch
+>
 	<slot />
 </div>
