7f502f8cf2ec81d339b9104b74fde35c78866286
cubedhuang <99danielh@gmail.com>
2022-11-26 00:30:31 -0500

    better css for colored checkbox

--- src/lib/components/ColoredCheckbox.svelte
+++ src/lib/components/ColoredCheckbox.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <button
-	class="relative flex items-center px-2 py-0.5 border border-gray-200 cursor-pointer rounded-lg overflow-hidden select-none hocus:border-gray-400 focus:outline-none transition-colors
+	class="flex items-center border border-gray-200 cursor-pointer rounded-lg overflow-hidden select-none hocus:border-gray-400 focus:outline-none transition-colors
 		dark:border-gray-800 dark:hocus:border-gray-700"
 	on:click={() => (checked = !checked)}
 	on:keypress|stopPropagation
@@ -13,10 +13,10 @@
 	aria-checked={checked}
 >
 	<span
-		class="{color} absolute top-0 bottom-0 left-0 w-4 transition-opacity"
+		class="{color} w-4 h-full transition-opacity"
 		class:opacity-100={checked}
 		class:opacity-30={!checked}
 	/>
 
-	<span class="ml-4">{label}</span>
+	<span class="px-2 py-0.5">{label}</span>
 </button>
