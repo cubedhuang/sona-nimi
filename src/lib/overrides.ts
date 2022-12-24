737ee7ee8f85c229fcc8e5fa74a2d482d8848990
Daniel Huang <99danielh@gmail.com>
2022-12-23 23:37:36 -0500

    update src/lib/overrides.ts

--- src/lib/overrides.ts
+++ src/lib/overrides.ts
@@ -15,8 +15,8 @@ export const newWords: Word[] = [
 	{
 		word: 'konsuno',
 		def: {
-			en: 'to gaslight, to deceive into believing a lie; the act of gaslighting'
-			tp: 'toki li lon ala; jan li toki pi lon ala'
+			en: 'to gaslight, to deceive into believing a lie; the act of gaslighting',
+			tok: 'toki li lon ala; jan li toki pi lon ala'
 		},
 		book: 'none',
 		coined_era: 'post-ku',
