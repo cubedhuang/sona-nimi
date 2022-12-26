2cbf10fe4061650a9b8a24c652bd7af1502111aa
cubedhuang <99danielh@gmail.com>
2022-12-25 21:12:47 -0500

    use nested local transitions

--- src/routes/(words)/WordDetails.svelte
+++ src/routes/(words)/WordDetails.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
 	import { createEventDispatcher } from 'svelte';
-	import { fly } from 'svelte/transition';
-
-	import { navigating } from '$app/stores';
+	import { scale, fly } from 'svelte/transition';
 
 	import type { Word } from '$lib/types';
 	import { language } from '$lib/stores';
@@ -23,127 +21,129 @@
 </script>
 
 {#if word}
-	{#key word.word}
-		<div
-			class="details"
-			transition:fly={{ y: 24, duration: $navigating ? 0 : 300 }}
-		>
-			<div class="flex items-end">
-				<h2 class="text-2xl">{word.word}</h2>
+	<div
+		class="fixed bottom-0 right-0"
+		transition:fly|local={{ y: 24, duration: 300 }}
+	>
+		{#key word.word}
+			<div class="details" transition:fly|local={{ y: 24, duration: 300 }}>
+				<div class="flex items-end">
+					<h2 class="text-2xl">{word.word}</h2>
 
-				<div class="ml-auto flex items-center gap-2">
-					<a href="/{word.word}" class="px-2 py-1 interactable">more</a>
+					<div class="ml-auto flex items-center gap-2">
+						<a href="/{word.word}" class="px-2 py-1 interactable">more</a>
 
-					<button
-						class="p-1 interactable"
-						on:click={() => {
-							word = null;
-						}}
-					>
-						<X />
-					</button>
-				</div>
-			</div>
-
-			<p class="faded">
-				{word.usage_category} &middot;
-				{getWordDisplayRecognition(word)}
-				{#if word.book !== 'none'}
-					&middot; {word.book}
-				{/if}
-				{#if word.coined_year}
-					&middot; {word.coined_year}
-				{/if}
-			</p>
-
-			<p class="mt-2">
-				{getWordDefinition(word, $language)}
-			</p>
-
-			{#if word.ku_data}
-				<p class="mt-2">
-					{word.ku_data}
-				</p>
-			{/if}
-
-			{#if word.see_also}
-				{@const words = word.see_also.split(', ')}
-
-				<p class="mt-2">
-					see
-					{#each words as other, i (other)}
-						<!-- Formatting here is weird to prevent additional spaces between commas -->
-						{i !== 0 ? ',' : ''}
-						<Link
-							href="#{other}"
+						<button
+							class="p-1 interactable"
 							on:click={() => {
-								dispatch('refer', other);
-							}}>{other}</Link
+								word = null;
+							}}
 						>
-					{/each}
+							<X />
+						</button>
+					</div>
+				</div>
+
+				<p class="faded">
+					{word.usage_category} &middot;
+					{getWordDisplayRecognition(word)}
+					{#if word.book !== 'none'}
+						&middot; {word.book}
+					{/if}
+					{#if word.coined_year}
+						&middot; {word.coined_year}
+					{/if}
 				</p>
-			{/if}
 
-			{#if word.creator || word.etymology}
-				<h3 class="mt-2 text-lg">origin</h3>
+				<p class="mt-2">
+					{getWordDefinition(word, $language)}
+				</p>
 
-				{#if word.etymology}
-					<p>
-						{word.source_language} &middot;
-						{word.etymology}
+				{#if word.ku_data}
+					<p class="mt-2">
+						{word.ku_data}
 					</p>
 				{/if}
 
-				{#if word.creator}
-					<p class="italic">
-						{word.creator}
+				{#if word.see_also}
+					{@const words = word.see_also.split(', ')}
+
+					<p class="mt-2">
+						see
+						{#each words as other, i (other)}
+							<!-- Formatting here is weird to prevent additional spaces between commas -->
+							{i !== 0 ? ',' : ''}
+							<Link
+								href="#{other}"
+								on:click={() => {
+									dispatch('refer', other);
+								}}>{other}</Link
+							>
+						{/each}
 					</p>
 				{/if}
-			{/if}
 
-			{#if word.sitelen_pona}
-				<h3 class="mt-2 text-lg">sitelen pona</h3>
+				{#if word.creator || word.etymology}
+					<h3 class="mt-2 text-lg">origin</h3>
 
-				<span class="font-medium font-pona text-4xl">
-					{word.sitelen_pona}
-				</span>
+					{#if word.etymology}
+						<p>
+							{word.source_language} &middot;
+							{word.etymology}
+						</p>
+					{/if}
 
-				{#if word.sitelen_pona_etymology}
-					<p>
-						{word.sitelen_pona_etymology}
+					{#if word.creator}
+						<p class="italic">
+							{word.creator}
+						</p>
+					{/if}
+				{/if}
+
+				{#if word.sitelen_pona}
+					<h3 class="mt-2 text-lg">sitelen pona</h3>
+
+					<span class="font-medium font-pona text-4xl">
+						{word.sitelen_pona}
+					</span>
+
+					{#if word.sitelen_pona_etymology}
+						<p>
+							{word.sitelen_pona_etymology}
+						</p>
+					{/if}
+				{/if}
+
+				{#if word.sitelen_sitelen}
+					<h3 class="mt-2 text-lg">sitelen sitelen</h3>
+
+					<img
+						src={word.sitelen_sitelen}
+						alt="{word.word} sitelen sitelen"
+						class="w-10 h-10 dark:invert"
+					/>
+				{/if}
+
+				{#if word.sitelen_emosi}
+					<h3 class="mt-2 text-lg">sitelen emosi</h3>
+
+					<p class="text-3xl">
+						{word.sitelen_emosi}
 					</p>
 				{/if}
-			{/if}
 
-			{#if word.sitelen_sitelen}
-				<h3 class="mt-2 text-lg">sitelen sitelen</h3>
+				{#if word.commentary}
+					<p class="mt-2 faded">
+						{word.commentary}
+					</p>
+				{/if}
 
-				<img
-					src={word.sitelen_sitelen}
-					alt="{word.word} sitelen sitelen"
-					class="w-10 h-10 dark:invert"
+				<span
+					class="absolute -top-4 -left-4 p-4 rounded-full {categoryColors[
+						word.usage_category
+					]}"
 				/>
-			{/if}
-
-			{#if word.sitelen_emosi}
-				<h3 class="mt-2 text-lg">sitelen emosi</h3>
-
-				<p class="text-3xl">
-					{word.sitelen_emosi}
-				</p>
-			{/if}
-
-			{#if word.commentary}
-				<p class="mt-2 faded">
-					{word.commentary}
-				</p>
-			{/if}
-
-			<span
-				class="absolute -top-4 -left-4 p-4 rounded-full {categoryColors[
-					word.usage_category
-				]}"
-			/>
-		</div>
-	{/key}
+			</div>
+		{/key}
+	</div>
 {/if}
--- src/routes/compounds/CompoundDetails.svelte
+++ src/routes/compounds/CompoundDetails.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
 	import { fly } from 'svelte/transition';
 
-	import { navigating } from '$app/stores';
-
 	import type { Compound } from '$lib/types';
 
 	import Link from '$lib/components/Link.svelte';
@@ -12,46 +10,51 @@
 </script>
 
 {#if compound}
-	{#key compound.compound}
-		<div
-			class="details top-auto max-h-80 md:max-h-[40rem]"
-			transition:fly={{ y: 24, duration: $navigating ? 0 : 300 }}
-		>
-			<div class="flex">
-				<h2 class="text-2xl">{compound.compound}</h2>
+	<div
+		class="fixed bottom-0 right-0"
+		transition:fly|local={{ y: 24, duration: 300 }}
+	>
+		{#key compound.compound}
+			<div
+				class="details top-auto max-h-80 md:max-h-[40rem]"
+				transition:fly|local={{ y: 24, duration: 300 }}
+			>
+				<div class="flex">
+					<h2 class="text-2xl">{compound.compound}</h2>
 
-				<button
-					class="ml-auto p-1 interactable"
-					on:click={() => {
-						compound = null;
-					}}
-				>
-					<X />
-				</button>
+					<button
+						class="ml-auto p-1 interactable"
+						on:click={() => {
+							compound = null;
+						}}
+					>
+						<X />
+					</button>
+				</div>
+
+				<p class="font-pona text-4xl">
+					{compound.glyphs?.join(' ') || compound.compound}
+				</p>
+
+				<p>
+					see
+					{#each compound.compound.split(' ') as word, i (word)}
+						{i !== 0 ? ',' : ''}
+						<Link href="/{word}">{word}</Link>
+					{/each}
+				</p>
+
+				<div class="flex flex-col mt-2">
+					{#each Object.entries(compound.uses) as [use, count]}
+						<p>
+							{use}
+							<span class="faded">
+								&middot; {count}%
+							</span>
+						</p>
+					{/each}
+				</div>
 			</div>
-
-			<p class="font-pona text-4xl">
-				{compound.glyphs?.join(' ') || compound.compound}
-			</p>
-
-			<p>
-				see
-				{#each compound.compound.split(' ') as word, i (word)}
-					{i !== 0 ? ',' : ''}
-					<Link href="/{word}">{word}</Link>
-				{/each}
-			</p>
-
-			<div class="flex flex-col mt-2">
-				{#each Object.entries(compound.uses) as [use, count]}
-					<p>
-						{use}
-						<span class="faded">
-							&middot; {count}%
-						</span>
-					</p>
-				{/each}
-			</div>
-		</div>
-	{/key}
+		{/key}
+	</div>
 {/if}
