23152ce15ce0fe24f553d470e44f7ccdeca71aa8
cubedhuang <99danielh@gmail.com>
2023-01-09 14:58:49 -0500

    make search sticky

--- src/lib/components/Search.svelte
+++ src/lib/components/Search.svelte
@@ -1,13 +1,36 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import X from './X.svelte';
 
 	export let placeholder: string;
 	export let value = '';
 
+	let div: HTMLDivElement;
 	let searchBar: HTMLInputElement;
+	let stick = false;
+
+	onMount(() => {
+		const observer = new IntersectionObserver(
+			([e]) => (stick = e.intersectionRatio < 1),
+			{ threshold: [1] }
+		);
+		observer.observe(div);
+
+		return () => observer.disconnect();
+	});
 </script>
 
-<div class="mt-1 flex gap-1 items-center">
+<div
+	bind:this={div}
+	class="sticky -top-1 z-10 w-fit max-w-full box-content -mt-2 pt-3 px-2 -mx-2 pb-2 -mb-2 flex gap-1 items-center rounded-b-lg bg-white transition
+		dark:bg-black ring-gray-800"
+	class:bg-opacity-0={!stick}
+	class:dark:bg-opacity-0={!stick}
+	class:shadow-lg={stick}
+	class:bg-opacity-100={stick}
+	class:dark:bg-opacity-100={stick}
+	class:dark:ring-1={stick}
+>
 	<input
 		type="text"
 		{placeholder}
