403bd01a082202155d3b8636fc6674be4b5186dc
cubedhuang <99danielh@gmail.com>
2023-08-09 22:38:05 -0400

    feat: add listen button to WordDetails

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -17,6 +17,7 @@ module.exports = {
 	plugins: [
 		plugin(function ({ addVariant }) {
 			addVariant('hocus', ['&:hover', '&:focus']);
+			addVariant('hocus-visible', ['&:hover', '&:focus-visible']);
 		})
 	]
 };
