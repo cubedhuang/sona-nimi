34e50c09c38511dff22beb8952b525e123db55a4
cubedhuang <99danielh@gmail.com>
2023-10-26 17:29:20 -0400

    add potential kala Asi audio

--- src/routes/(words)/WordDetails.svelte
+++ src/routes/(words)/WordDetails.svelte
@@ -26,6 +26,11 @@
 	function play() {
 		audio?.play();
 	}
+
+	$: audioUrl =
+		possibleWord?.audio?.['jan_lakuse'] ??
+		possibleWord?.audio?.['kala_asi'] ??
+		null;
 </script>
 
 <Details bind:value={possibleWord} key={word => word.id} let:value={word}>
@@ -35,8 +40,8 @@
 		<div class="ml-auto flex items-center gap-2">
 			<a href="/{word.id}" class="px-2 py-1 interactable">more</a>
 
-			{#if word.audio?.['jan_lakuse']}
-				<audio src={word.audio['jan_lakuse']} bind:this={audio} />
+			{#if audioUrl}
+				<audio src={audioUrl} bind:this={audio} />
 
 				<button class="p-1 interactable" on:click={play}>
 					<svg
