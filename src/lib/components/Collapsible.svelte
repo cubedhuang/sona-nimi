1a90145b2ade99b152ecea9a057bc618346f505e
cubedhuang <99danielh@gmail.com>
2023-11-03 20:14:29 -0400

    better tabbing styles

--- src/lib/components/Collapsible.svelte
+++ src/lib/components/Collapsible.svelte
@@ -7,15 +7,21 @@
 	let expanded = false;
 
 	$: shownContent = expanded ? content : content.slice(0, length);
+
+	let button: HTMLButtonElement;
 </script>
 
 {shownContent}
 
 {#if collapsible}
 	<button
-		class="faded underline underline-offset-2 decoration-transparent hocus-visible:decoration-current transition"
+		class="faded underline underline-offset-2 decoration-transparent hocus-visible:decoration-current outline-none outline-offset-4 focus-visible:outline-gray-500 rounded transition"
 		class:block={expanded}
-		on:click={() => (expanded = !expanded)}
+		on:click={() => {
+			expanded = !expanded;
+			button.blur();
+		}}
+		bind:this={button}
 	>
 		{expanded ? 'less' : '...more'}
 	</button>
--- src/lib/components/Link.svelte
+++ src/lib/components/Link.svelte
@@ -6,7 +6,7 @@
 	{href}
 	target={href.startsWith('/') || href.startsWith('#') ? undefined : '_blank'}
 	rel="noopener noreferrer"
-	class="text-blue-500 hocus:text-blue-600 transition-colors"
+	class="text-blue-500 hocus:text-blue-600 transition-colors outline-none outline-offset-4 focus-visible:outline-current rounded"
 	on:click
 >
 	<slot />
