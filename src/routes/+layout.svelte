5aa7e2c2ea9f5a29b8d97ba472dbfb5d6b00416a
cubedhuang <99danielh@gmail.com>
2023-11-19 17:07:15 -0500

    improve light/dark mode default

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -66,29 +66,9 @@
 			showSpinner: false
 		});
 	});
-
-	let innerWidth: number;
-	// only show the option if the screen is larger than xl
-	$: showWidthOption = innerWidth && innerWidth > 1280;
 </script>
 
-<svelte:head>
-	<script>
-		{
-			const darkModeValue = localStorage.getItem('darkMode');
-
-			if (darkModeValue !== null) {
-				document.documentElement.classList.toggle(
-					'dark',
-					darkModeValue === 'true'
-				);
-			}
-		}
-	</script>
-</svelte:head>
-
 <svelte:window
-	bind:innerWidth
 	on:click={() => {
 		opened = false;
 	}}
@@ -192,51 +172,49 @@
 				</button>
 			{/if}
 
-			{#if showWidthOption}
-				<button
-					class="{commonClasses} {hoverableClasses} cursor-pointer"
-					on:click={() => {
-						if ($screenWidth === 'full') {
-							$screenWidth = 'large';
-						} else {
-							$screenWidth = 'full';
-						}
-					}}
-					aria-hidden="true"
-				>
-					{#if $screenWidth === 'large'}
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke-width="1.5"
-							stroke="currentColor"
-							class="w-6 h-6"
-						>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15"
-							/>
-						</svg>
-					{:else}
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke-width="1.5"
-							stroke="currentColor"
-							class="w-6 h-6"
-						>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								d="M9 9V4.5M9 9H4.5M9 9L3.75 3.75M9 15v4.5M9 15H4.5M9 15l-5.25 5.25M15 9h4.5M15 9V4.5M15 9l5.25-5.25M15 15h4.5M15 15v4.5m0-4.5l5.25 5.25"
-							/>
-						</svg>
-					{/if}
-				</button>
-			{/if}
+			<button
+				class="max-lg:hidden {commonClasses} {hoverableClasses} cursor-pointer"
+				on:click={() => {
+					if ($screenWidth === 'full') {
+						$screenWidth = 'large';
+					} else {
+						$screenWidth = 'full';
+					}
+				}}
+				aria-hidden="true"
+			>
+				{#if $screenWidth === 'large'}
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="w-6 h-6"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15"
+						/>
+					</svg>
+				{:else}
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="w-6 h-6"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M9 9V4.5M9 9H4.5M9 9L3.75 3.75M9 15v4.5M9 15H4.5M9 15l-5.25 5.25M15 9h4.5M15 9V4.5M15 9l5.25-5.25M15 15h4.5M15 15v4.5m0-4.5l5.25 5.25"
+						/>
+					</svg>
+				{/if}
+			</button>
 
 			<button
 				class="{commonClasses} {hoverableClasses} cursor-pointer"
