6b6ed48484470618a4efaa95b51e87c2d213be49
cubedhuang <99danielh@gmail.com>
2024-02-12 19:58:39 -0500

    remove other luka pona color

--- src/routes/luka-pona/LukaPonaEntry.svelte
+++ src/routes/luka-pona/LukaPonaEntry.svelte
@@ -9,15 +9,9 @@
 		<img
 			src={word.luka_pona?.gif}
 			alt="{word.word} luka pona"
-			class="rounded-lg w-full aspect-video bg-blue-100 dark:bg-blue-950"
+			class="rounded-lg w-full aspect-video bg-gray-100 dark:bg-gray-900"
 			loading="lazy"
 		/>
-	{:else}
-		<div
-			class="h-full aspect-video bg-gray-100 dark:bg-gray-900 rounded-lg"
-		>
-			<p class="sr-only">no luka pona available</p>
-		</div>
 	{/if}
 
 	<b class="block mt-2 group-hocus-visible:text-blue-500 transition">
