d34a2990513295a3cccc34b561aed78bf6935e29
cubedhuang <99danielh@gmail.com>
2024-02-16 09:01:11 -0500

    heavily refactor lukaponaentry

--- src/routes/luka-pona/LukaPonaEntry.svelte
+++ src/routes/luka-pona/LukaPonaEntry.svelte
@@ -7,7 +7,6 @@
 
 	export let word: Word;
 
-	let container: HTMLButtonElement;
 	let img: HTMLImageElement;
 	let canvas: HTMLCanvasElement;
 	let ctx: CanvasRenderingContext2D | null = null;
@@ -20,7 +19,7 @@
 		});
 	}
 
-	function onload(e: Event | HTMLElement) {
+	function handleLoaded() {
 		if (!ctx) {
 			ctx = canvas.getContext('2d');
 		}
@@ -28,10 +27,6 @@
 		canvas.width = img.width;
 		canvas.height = img.height;
 		ctx?.drawImage(img, 0, 0, img.width, img.height);
-
-		const element = 'target' in e ? e.target : e;
-
-		element?.removeEventListener('load', onload);
 	}
 
 	function handleEnter() {
@@ -54,27 +49,21 @@
 
 	onMount(() => {
 		if (img.complete) {
-			onload(img);
+			handleLoaded();
 		}
-
-		container.addEventListener('mouseenter', handleEnter);
-		container.addEventListener('mouseleave', handleLeave);
-		container.addEventListener('focus', handleEnter);
-		container.addEventListener('blur', handleLeave);
-		container.addEventListener('touchstart', handleEnter, {
-			passive: true
-		});
-		container.addEventListener('touchend', handleLeave, {
-			passive: true
-		});
 	});
 </script>
 
 <button
 	class="text-left group outline-none focus-visible:outline-gray-500"
-	on:click
 	id={word.id}
-	bind:this={container}
+	on:click
+	on:mouseenter={handleEnter}
+	on:mouseleave={handleLeave}
+	on:focus={handleEnter}
+	on:blur={handleLeave}
+	on:touchstart={handleEnter}
+	on:touchend={handleLeave}
 >
 	{#if word.luka_pona?.gif}
 		<div class="relative">
@@ -91,7 +80,7 @@
 				class:opacity-0={!$autoplay}
 				loading="lazy"
 				bind:this={img}
-				on:load={onload}
+				on:load|once={handleLoaded}
 			/>
 		</div>
 	{/if}
