12fee98d2bdc317321feb4905df19ab365fba312
cubedhuang <99danielh@gmail.com>
2024-02-16 00:03:00 -0500

    make fly distance smaller

--- src/lib/components/Details.svelte
+++ src/lib/components/Details.svelte
@@ -23,11 +23,11 @@
 {#if value}
 	<div
 		class="z-20 fixed bottom-0 right-0 w-full"
-		transition:fly|local={{ y: 24, duration: 300 }}
+		transition:fly|local={{ y: 8, duration: 300 }}
 	>
 		{#key key(value)}
 			<div
-				transition:fly|local={{ y: 24, duration: 300 }}
+				transition:fly|local={{ y: 8, duration: 300 }}
 				class="absolute bottom-0 right-0 left-0 max-h-[75vh] p-6 overflow-y-auto bg-white border-t border-gray-400 shadow-lg
 					md:bottom-4 md:right-4 md:left-auto md:top-auto md:w-[36rem] md:max-h-[40rem] md:border md:rounded-lg
 					dark:bg-black dark:border-gray-800"
