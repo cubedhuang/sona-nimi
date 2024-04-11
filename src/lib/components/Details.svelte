90e2a0d7398e5beb2cc93d7756bb3c243ef037fc
cubedhuang <99danielh@gmail.com>
2024-04-11 11:24:16 -0400

    fix details again

--- src/lib/components/Details.svelte
+++ src/lib/components/Details.svelte
@@ -29,7 +29,7 @@
 			<div
 				transition:fly|local={{ y: 16, duration: 300 }}
 				class="absolute bottom-0 right-0 left-0 max-h-[75vh] p-6 overflow-y-auto bg-white border-t border-gray-400 shadow-lg
-					md:top-4 md:bottom-4 md:right-4 md:left-auto md:mt-auto md:w-[36rem] md:max-h-[40rem] md:border md:rounded-lg
+					md:top-4 md:bottom-4 md:right-4 md:left-auto md:h-fit md:mt-auto md:w-[36rem] md:max-h-[40rem] md:border md:rounded-lg
 					dark:bg-black dark:border-gray-800"
 			>
 				<slot value={nonNullValue} />
