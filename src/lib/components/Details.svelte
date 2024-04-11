8956e2e32b7a7ac3ab67e45c14d2b55f3005252c
cubedhuang <99danielh@gmail.com>
2024-04-11 11:18:19 -0400

    fix #18 word info box cutoff

--- src/lib/components/Details.svelte
+++ src/lib/components/Details.svelte
@@ -22,14 +22,14 @@
 
 {#if value}
 	<div
-		class="z-20 fixed bottom-0 right-0 w-full"
+		class="z-20 fixed top-0 bottom-0 right-0 w-full"
 		transition:fly|local={{ y: 16, duration: 300 }}
 	>
 		{#key key(value)}
 			<div
 				transition:fly|local={{ y: 16, duration: 300 }}
 				class="absolute bottom-0 right-0 left-0 max-h-[75vh] p-6 overflow-y-auto bg-white border-t border-gray-400 shadow-lg
-					md:bottom-4 md:right-4 md:left-auto md:top-auto md:w-[36rem] md:max-h-[40rem] md:border md:rounded-lg
+					md:top-4 md:bottom-4 md:right-4 md:left-auto md:mt-auto md:w-[36rem] md:max-h-[40rem] md:border md:rounded-lg
 					dark:bg-black dark:border-gray-800"
 			>
 				<slot value={nonNullValue} />
