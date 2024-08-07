4c445c27495c7fb990e838e3a75ed2ecb3e28119
cubedhuang <99danielh@gmail.com>
2024-08-06 23:40:40 -0400

    new lipamanka semantic spaces format

--- src/routes/data/lipamanka/+server.ts
+++ src/routes/data/lipamanka/+server.ts
@@ -9,32 +9,16 @@ export async function GET({ fetch, setHeaders }) {
 		throw error(500, 'Failed to fetch data');
 	}
 
-	const rawDictionary = rawText
-		.split('<h2 id="the-dictionary">The Dictionary</h2>')[1]
-		.split('<br><br><br>', 1)[0]
-		.trim();
+	const re =
+		/<details>\s*?<summary id="([^"]+)">.*?<\/summary>\s*([\s\S]*?)\s*<\/details>/g;
 
 	const words: Record<string, string> = {};
 
-	for (const entry of rawDictionary.split('<h3 id="')) {
-		if (!entry) {
-			continue;
-		}
+	let match: RegExpExecArray | null;
 
-		const [word, ...rest] = entry.split('">');
-
-		const def = rest
-			.join('">')
-			.slice(word.length + '</h3>'.length)
-			.trim();
-
-		if (word === 'mije-and-meli') {
-			words.mije = def;
-			words.meli = def;
-			continue;
-		}
-
-		words[word] = def;
+	while ((match = re.exec(rawText))) {
+		const [, word, definition] = match;
+		words[word] = definition;
 	}
 
 	setHeaders({ 'Cache-Control': 's-maxage=3600' });
