1c655921018cda573b7db81a6d7b5f387f7b9243
cubedhuang <99danielh@gmail.com>
2024-08-18 01:18:58 -0400

    improve about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -31,97 +31,109 @@
 	</button>
 </p>
 
-<div class="leading-relaxed">
-	<h1>{t('sona', 'about')} &ndash; nimi.li</h1>
+<h1>{t('sona', 'about')} &ndash; nimi.li</h1>
 
-	<p>
-		{t('mi', "Hi! I'm")}
-		<Link href="https://github.com/cubedhuang/">ilo Tani</Link>{t(
-			' li pali e ni. jan',
-			', and I made this project. Thanks to'
-		)}
-		<Link href="https://github.com/woflydev/">woflydev</Link>
-		{t(
-			'li ken e ni: sina awen e lipu lon ilo sina. sina wile pona e lipu ni la, o kepeken ilo',
-			'for making this a Progressive Web Application. This is an'
-		)}
-		<Link href="https://github.com/cubedhuang/sona-nimi"
-			>{t('GitHub', 'open source project on GitHub')}</Link
-		>!
-	</p>
+<div class="mt-4 grid gap-4 leading-relaxed md:grid-cols-2">
+	<div class="alert">
+		<h2>{t('mi', 'me')}</h2>
 
-	<p>
-		{t('ilo pona mute li ken e lipu ni. pali', '')}
-		<strong>nimi.li</strong>
-		{t(
-			'la,',
-			'was built with data from many amazing projects. Thanks to all of the following projects for their work:'
-		)}
-	</p>
-
-	<ul>
-		<li>
-			<Link href="https://linku.la/about/"
-				>{t('sona Linku', "Linku's public API")}</Link
-			>
-			{t('en', 'and')}
-			<Link href="https://lipamanka.gay/essays/dictionary"
-				>{t(
-					'lipu lipamanka',
-					"lipamanka's semantic space dictionary"
-				)}</Link
-			>
-			{t(
-				'li kama e sona nimi.',
-				'is used for all of data with the vocabulary.'
+		<p>
+			{t('mi', "Hi! I'm")}
+			<Link href="https://github.com/cubedhuang/">ilo Tani</Link>{t(
+				' li pali e ni. jan',
+				', and I made this project. Thanks to'
 			)}
-		</li>
+			<Link href="https://github.com/woflydev/">woflydev</Link>
+			{t(
+				'li ken e ni: sina awen e lipu lon ilo sina. sina wile pona e lipu ni la, o kepeken ilo',
+				'for making this a Progressive Web Application. This is an'
+			)}
+			<Link href="https://github.com/cubedhuang/sona-nimi"
+				>{t('GitHub', 'open source project on GitHub')}</Link
+			>!
+		</p>
+	</div>
 
-		<li>
-			{t('mi kepeken', "Sonja Lang's")}
-			<Link href="https://tokipona.org/compounds.txt"
-				>{t('sona ku lili', 'public subsets of ku')}</Link
-			>
-			{t('pi jan Sonja tawa ilo ku.', 'were used for ilo ku.')}
-		</li>
+	<div class="alert">
+		<h2>{t('ilo pali', 'tools')}</h2>
 
-		<li>
-			{t('mi sitelen pona e nimi kepeken', 'The')}
-			<Link
-				href="https://www.kreativekorp.com/software/fonts/sitelenselikiwen/"
-			>
-				{t('sitelen seli kiwen', 'sitelen seli kiwen font')}</Link
-			>{t('.', ' is used for sitelen pona displayed on the site.')}
-		</li>
-	</ul>
+		<p>
+			{t('mi pali e lipu ni kepeken ilo', 'This website was built with')}
+			<Link href="https://kit.svelte.dev">SvelteKit</Link>{t(
+				' kepeken ilo',
+				','
+			)}
+			<Link href="https://www.typescriptlang.org">TypeScript</Link>{t(
+				' kepeken ilo',
+				','
+			)}
+			<Link href="https://tailwindcss.com">TailwindCSS</Link>{t(
+				' kepeken ilo',
+				', and'
+			)}
+			<Link href="https://vite-pwa-org.netlify.app/">Vite PWA</Link>.
+		</p>
+	</div>
 
-	<h2>{t('lipu ni', 'this website')}</h2>
+	<div class="alert">
+		<h2>{t('tan sona', 'sources')}</h2>
 
-	<p>
-		{t('mi pali e lipu ni kepeken ilo', 'This website was built with')}
-		<Link href="https://kit.svelte.dev">SvelteKit</Link>{t(
-			' kepeken ilo',
-			','
-		)}
-		<Link href="https://www.typescriptlang.org">TypeScript</Link>{t(
-			' kepeken ilo',
-			','
-		)}
-		<Link href="https://tailwindcss.com">TailwindCSS</Link>{t(
-			' kepeken ilo',
-			', and'
-		)}
-		<Link href="https://vite-pwa-org.netlify.app/">Vite PWA</Link>.
-	</p>
+		<p>
+			{t('ilo pona mute li ken e lipu ni. pali', '')}
+			<strong>nimi.li</strong>
+			{t(
+				'la,',
+				'was built with data from many amazing projects. Thanks to all of the following projects for their work:'
+			)}
+		</p>
 
-	<h3>{t('ante', 'differences')}</h3>
+		<ul>
+			<li>
+				<Link href="https://linku.la/about/"
+					>{t('sona Linku', "Linku's public API")}</Link
+				>
+				{t('en', 'and')}
+				<Link href="https://lipamanka.gay/essays/dictionary"
+					>{t(
+						'lipu lipamanka',
+						"lipamanka's semantic space dictionary"
+					)}</Link
+				>
+				{t(
+					'li kama e sona nimi.',
+					'is used for all of data with the vocabulary.'
+				)}
+			</li>
 
-	<p>
-		{t(
-			'ijo mute a lipu li tan sona Linku. taso, mi wan e sona Linku e sona lipamanka la jan li ken lukin e ona tu kepeken lipu wan. (lipamanka li ken e ni.)',
-			"Almost all data on this website is directly from sona Linku. However, I'm also scraping lipamanka's semantic space dictionary (with permission) to make the entries immediately available here."
-		)}
-	</p>
+			<li>
+				{t('mi kepeken', "Sonja Lang's")}
+				<Link href="https://tokipona.org/compounds.txt"
+					>{t('sona ku lili', 'public subsets of ku')}</Link
+				>
+				{t('pi jan Sonja tawa ilo ku.', 'were used for ilo ku.')}
+			</li>
+
+			<li>
+				{t('mi sitelen pona e nimi kepeken', 'The')}
+				<Link
+					href="https://www.kreativekorp.com/software/fonts/sitelenselikiwen/"
+				>
+					{t('sitelen seli kiwen', 'sitelen seli kiwen font')}</Link
+				>{t('.', ' is used for sitelen pona displayed on the site.')}
+			</li>
+		</ul>
+	</div>
+
+	<div class="alert">
+		<h2>{t('ante', 'differences')}</h2>
+
+		<p>
+			{t(
+				'ijo mute a lipu li tan sona Linku. taso, mi wan e sona Linku e sona lipamanka la jan li ken lukin e ona tu kepeken lipu wan. (lipamanka li ken e ni.)',
+				"Almost all data on this website is directly from sona Linku. However, I'm also scraping lipamanka's semantic space dictionary (with permission) to make the entries immediately available here."
+			)}
+		</p>
+	</div>
 </div>
 
 <style lang="postcss">
@@ -130,18 +142,18 @@
 	}
 
 	h2 {
-		@apply mt-4 text-3xl;
+		@apply text-xl;
 	}
 
-	h3 {
-		@apply mt-4 text-xl;
-	}
-
-	p {
+	div p {
 		@apply mt-2;
 	}
 
 	ul {
 		@apply ml-4 mt-2 list-disc;
+	}
+
+	.alert {
+		@apply p-4;
 	}
 </style>
